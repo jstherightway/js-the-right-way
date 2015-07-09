b3dfa313db38f5017a73a81798bffbbe8548cc0b
allanesquina <loloudxur@gmail.com>
2015-07-09 13:45:57 -0300

    i18n structure

--- scripts/tasks/i18n.js
+++ scripts/tasks/i18n.js
@@ -23,28 +23,56 @@ module.exports = function (grunt) {
   var generete = function () {
     var template = grunt.file.read(path.join(TEMPLATE_PATH, TEMPLATE_FILE));
     var data, t;
-    var defaultData = grunt.file.readJSON(path.join(JSON_PATH,DEFAULT_LANGUAGE));
+    var defaultData = grunt.file.readJSON(path.join(JSON_PATH, DEFAULT_LANGUAGE.replace('.json', ''),DEFAULT_LANGUAGE));
 
     grunt.file.recurse( JSON_PATH, function(abspath, rootdir, subdir, filename){
-      grunt.log.write(filename + '\n');
-      data = grunt.file.readJSON(path.join(JSON_PATH,filename));
+      // grunt.log.write(filename + '\n');
+      if(filename.indexOf('.json') !== -1) {
 
-      // Extend from default language
-      data = extend(true, defaultData, data);
+        data = getData(path.join(JSON_PATH, filename.replace('.json', ''), filename), filename.replace('.json', ''));
+        // grunt.log.write(data.goodParts.title)
 
-      t = grunt.template.process(template, {data: data});
-      save(TEMPLATE_FILE, filename, t);
+        // Extend from default language
+        data = extend(true, defaultData, data);
+
+        t = grunt.template.process(template, {data: data});
+        save(TEMPLATE_FILE, filename, t);
+      }
     });
   }
 
   // Save a file with the rendered template
   var save = function (name, dest, content) {
       try {
-        grunt.log.write('Creating file ' + path.join(PUBLIC_PATH, dest.replace('.json', ''), name) + '\n');
+        // grunt.log.write('Creating file ' + path.join(PUBLIC_PATH, dest.replace('.json', ''), name) + '\n');
         grunt.file.write(path.join(PUBLIC_PATH, dest.replace('.json', ''), name), content);
       } catch (e) {
         grunt.log.write(e);
       }
+  }
+
+  var getData = function(filename, lang) {
+    var data = grunt.file.readJSON(filename);
+    var re = /{{@link=([^\s]+)}}/, res;
+    data = (function r(o) {
+      var tmp;
+        if( typeof o == "object" ) {
+          for (var v in o) {
+            if (o.hasOwnProperty(v)) {
+                o[v] = r(o[v]);
+            }
+          }
+        }
+        else {
+            res= re.exec(o);
+            if(res) {
+              tmp = grunt.file.read(path.join(JSON_PATH,lang,res[1]));
+              o = tmp !== '' ? tmp : 'File ' + res[1] + ' not found.'
+            }
+        }
+        return o;
+    }(data));
+    return data
   }
 
   grunt.registerTask('i18n', function() {
